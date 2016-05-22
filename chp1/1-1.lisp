e6613b06879bd5bd250addc698d336a2ae7e848e
Spencer Dixon <spencercdixon@gmail.com>
2016-05-22 11:59:12 -0400

    Initial commit.  Completes first exercise

---
+++ chp1/1-1.lisp
@@ -0,0 +1,21 @@
+10                              ; 10
+(+ 5 3 4 )                      ; 12
+(- 9 1)                         ; 8
+(/ 6 2)                         ; 3
+(+ (* 2 4) (- 4 6))             ; 6
+(define a 3)                    ; undefined - just sets value of 3 to a
+(define b (+ a 1))              ; undefined - sets value of 4 to b
+(+ a b (* a b))                 ; 3 + 4 + 12 = 19
+(= a b)                         ; f# - false
+(if (and (> b a) (< b (* a b)))
+  b
+  a)                            ; 4 (b)
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))                ; 16
+(+ 2 (if (> b a) b a))          ; 6
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))                     ; 16
+
